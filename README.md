4929dddd711f71bb810f13a90714d7b1702c76df
Ted Leary <ted.e.leary@ey.com>
2020-10-19 15:02:59 -0700

    chore(webpack): finalize verbose webpack setup

---
+++ README.md
@@ -0,0 +1,7 @@
+## Folder & File Structure for Webpack - 
+
+Component folders are at the root level
+
+Components should be in a folder sharing the same name as their js file
+
+Components should be accompanied by an index.scss and an index.html file
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
@@ -5,53 +6,85 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 
-const components = ['testOne', 'testTwo']
+const entries = [];
+const htmlFiles = [];
 
-const multipleHtmlFiles = components.map((entryName) => {
+// read components directory
+
+const componentsContents = fs.readdirSync(`${__dirname}/components`);
+
+// read component directories
+
+for (let i = 0; i < componentsContents.length; i++) {
+  const componentDirectory = componentsContents[i];
+
+  if (componentDirectory.match(/\./)) {
+    // i'm a file don't try to readdir me
+
+    console.log(`ignore me i'm a file - ${componentDirectory}`);
+  } else {
+    // i'm a directory readdir me
+
+    const componentContents = fs.readdirSync(
+      `${__dirname}/components/${componentDirectory}`
+    );
+    for (let i = 0; i < componentContents.length; i++) {
+      const file = componentContents[i];
+      if (file.match(/index\.js/)) {
+        entries.push(`${componentDirectory}/${file}`);
+      }
+      if (file.match(/index\.html/)) {
+        htmlFiles.push(`${componentDirectory}/${file}`);
+      }
+    }
+  }
+}
+
+// generates component html files
+const multipleHtmlFiles = htmlFiles.map((entryName) => {
+  const [directory, fileName] = entryName.split("/");
   return new HtmlWebpackPlugin({
-    filename: `components/${entryName}/index.html`,
-    template: `./${entryName}/index.html`,
-    chunks: [entryName]
-  })
-})
+    filename: `components/${directory}/${fileName}`,
+    template: `./components/${directory}/${fileName}`,
+    chunks: [directory],
+  });
+});
 
-console.log(__dirname + '/dist/components')
+// generates component entries
+const multipleEntries = entries.reduce((acc, curr) => {
+  const [directory, fileName] = curr.split("/");
+  return { ...acc, [directory]: `./components/${directory}/${fileName}` };
+}, {});
 
 module.exports = {
   watch: true,
-  entry: {
-    testOne: "./testOne/testOne.js",
-    testTwo: "./testTwo/testTwo.js",
-  },
+  entry: multipleEntries,
   output: {
-    filename: "components/[name]/[name].js",
+    filename: "components/[name]/index.js",
     path: path.resolve(__dirname, "dist"),
-    publicPath: ""
+    publicPath: "",
   },
   module: {
     rules: [
       {
         test: /\.s[ac]ss$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
     ],
   },
   optimization: {
     minimize: true,
-    minimizer: [
-      new CssMinimizerPlugin(),
-      new TerserPlugin()
-    ]
+    minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
   },
   devServer: {
-    contentBase: path.resolve(__dirname, 'dist'),
-    port: 9000
+    contentBase: path.resolve(__dirname, "dist"),
+    port: 9000,
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: 'components/[name]/[name].css'
+      filename: "components/[name]/index.css",
     }),
     new CleanWebpackPlugin(),
-    ...multipleHtmlFiles
-  ]
+    ...multipleHtmlFiles,
+  ],
 };
